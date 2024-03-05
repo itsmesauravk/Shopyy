b9761c539f26109ddc2bff50d29a933f9dbd9486
itsmesauravk <sauravkarki10.12@gmail.com>
2024-03-05 22:23:52 +0545

    updated

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -4,7 +4,15 @@ import './ListProduct.css'
 const ListProduct = () => {
   return (
     <div className='listproduct'>
-        ListProduct
+        <h1>All Product List</h1>
+        <div className="listproduct-format-main">
+          <p>Products</p>
+          <p>Title</p>
+          <p>Old Price</p>
+          <p>New Price</p>
+          <p>Category</p>
+          <p>Remove</p>
+        </div>
     </div>
   )
 }
